ef5b83e4d5c10dd953705f4f48f2af54e9c51cc5
datoyacx <datoyacx@outlook.jp>
2022-01-26 02:10:14 +0700

    add util

---
+++ lib/util.ts
@@ -0,0 +1,10 @@
+import { Types } from 'mongoose';
+
+export function isValidObjectId(id: string){
+    if(Types.ObjectId.isValid(id)){
+        if((String)(new Types.ObjectId(id)) === id)
+            return true;        
+        return false;
+    }
+    return false;
+}
